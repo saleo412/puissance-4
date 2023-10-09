62e8498e7b296fd6881d75f595dfc08f591ed004
GARY Felicite <felicite.gary@etu.univ-amu.fr>
2023-10-09 11:59:45 +0200

    travail

--- tp3/main.cpp
+++ tp3/main.cpp
@@ -4,7 +4,34 @@
 using namespace std;
 
 
+int jouer(int tab)
+{
+    int i=5;
+    int j;
+    bool j1=true,j2=false,n;
+    while(gagne(tab) == True)
+    {
+        cout << "entrez la colonne souhaité" << endl;
+        cin >> j;
+        while(tab[i][j] != 0)
+        {
+            i -= 1;
 
+        }
+        if(j1 == true)
+        {
+            tab[i][j] = 1;
+        }
+        else
+        {
+            tab[i][j] = 2;
+        }
+        j1=n;
+        j1 = j2;
+        j2=n;
+    }
+
+}
 
 int main()
 {
